cde81922626d057d62794638fa46ee5ab7c914a6
wandaafifah <42725331+wandaafifah@users.noreply.github.com>
2018-10-01 10:48:30 +0700

    Update registrasi.php

--- registrasi.php
+++ registrasi.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Form Registrasi</title>
+	<title></title>
 </head>
 <body>
-
-
-
+	<form action="prosesregis.php" method="POST">
+		Nim :<input type="text" name="nim" ><br></br>
+		Nama :<input type="text" name="nama"><br></br>
+		Email :<input type="email" name="email"><br></br>
+	<input type="submit" value="kirim">
 </body>
 </html>
