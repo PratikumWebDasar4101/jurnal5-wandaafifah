1e34e0b8a3b956453999c332933fee376b02cf85
wandaafifah <42725331+wandaafifah@users.noreply.github.com>
2018-10-01 10:49:21 +0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -1,7 +1,24 @@
 <?php
-
-
-
-
-
+	$nim =$_POST['nim'];
+	$nama =$_POST['nama'];
+	$email =$_POST['email'];
+	$reinput = "<br><a href='registrasi.php'> RE-INPUT</a>";
+	$enter = "<br>";
+	if(strlen($nama) > 10){
+		echo"maksimal 25 karakter";
+		echo $reinput;
+		echo $enter;
+	}
+	if(!is_numeric($nim) || strlen((string)$nim) > 10){
+		echo "nim harus berjumlah 10 karakter";
+		echo $reinput;
+		echo $enter;
+	}
+	if(!preg_match("/^[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z.]{2,5}$/",$email)){
+		echo "format salah";
+		echo $reinput;
+		echo $enter;
+	}else{
+		header("Location: komentar.php");
+	}
 ?>
